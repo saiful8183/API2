b1105937c81029bf5cbcd99fee8829dc0fd1daab
Saiful8183 <Manha103019@gmail.com>
2024-11-08 11:33:01 -0500

    My LatestApi

--- src/test/java/api/Test/UserTests2UserEndPointsRoutesImplimentataion2ForPropertiesFile.java
+++ src/test/java/api/Test/UserTests2UserEndPointsRoutesImplimentataion2ForPropertiesFile.java
@@ -126,5 +126,16 @@ public class UserTests2UserEndPointsRoutesImplimentataion2ForPropertiesFile {
 		logger.info("===========  User deleted==================");
 
 	}
+	@Test(priority = 4)
+	public void testDeleteUserByName2() {
+		
+		
+		logger.info("=========== Deleting User==================");
+		
+		Response response = UserEndPointsRoutesImplimentataion2ForPropertiesFile.DeleteUser(this.userPaylod.getUsername());
+		AssertJUnit.assertEquals(response.getStatusCode(),404);
+		logger.info("===========  User deleted==================");
+		
+	}
 
 }
